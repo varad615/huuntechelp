ba9e5bd365de24e9b42e9804384762b72cf87bd3
Varad Gundap <30592638+varad615@users.noreply.github.com>
2025-04-17 00:30:22 +0530

    test main

--- lib/main.dart
+++ lib/main.dart
@@ -1,54 +1,45 @@
-import 'dart:async';
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:sentry_flutter/sentry_flutter.dart';
-
-import 'package:hunttechelp/pages/main_page.dart';
-import 'util.dart';
-import 'theme.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await SentryFlutter.init(
     (options) {
-      options.dsn =
-          'https://e07448e458268dad33d285f3719b57af@o4509161794961408.ingest.de.sentry.io/4509161796534352';
-      options.sendDefaultPii = true;
+      options.dsn = 'YOUR_DSN_HERE';
+      options.debug = true;
+      options.diagnosticLevel = SentryLevel.debug;
       options.tracesSampleRate = 1.0;
       options.profilesSampleRate = 1.0;
+      options.sendDefaultPii = true;
     },
-    appRunner: () => runZonedGuarded(
-      () async {
-        await Firebase.initializeApp(); // Firebase after Sentry init
-        runApp(
-          SentryWidget(
-            child: MyApp(),
-          ),
-        );
-      },
-      (error, stackTrace) async {
-        // Capture any unhandled errors
-        await Sentry.captureException(error, stackTrace: stackTrace);
-      },
+    appRunner: () => runApp(
+      SentryWidget(
+        child: const HelloWorldApp(),
+      ),
     ),
   );
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class HelloWorldApp extends StatelessWidget {
+  const HelloWorldApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    TextTheme textTheme = createTextTheme(context, "ABeeZee", "ABeeZee");
-    MaterialTheme theme = MaterialTheme(textTheme);
-
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Health App',
-      theme: theme.light(),
-      home: const MainPage(),
+      home: Scaffold(
+        appBar: AppBar(title: const Text('Sentry Test')),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: () {
+              // Force an error to test if Sentry logs it
+              throw Exception(
+                  "👋 This is a test exception from Hello World app.");
+            },
+            child: const Text('Throw Test Error'),
+          ),
+        ),
+      ),
     );
   }
 }
